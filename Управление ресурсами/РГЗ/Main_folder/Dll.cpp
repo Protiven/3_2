2de54418fea013ea5b7bb2327f00a41dd9119ee5
Protiven <39469340+Protiven@users.noreply.github.com>
2020-05-27 16:19:25 +0700

    UP RGZ

--- Управление ресурсами/РГЗ/Main_folder/Dll.cpp
+++ Управление ресурсами/РГЗ/Main_folder/Dll.cpp
@@ -1,5 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <windows.h>
+#include <iostream>
+#include <string.h>
+#include <intrin.h>
 
 //If the network is exist, then function return 1 in first position. Else return 0.
 extern "C" _declspec(dllexport) void  check_network(TCHAR* val) 
@@ -7,30 +10,63 @@ extern "C" _declspec(dllexport) void  check_network(TCHAR* val)
 	int value = GetSystemMetrics(SM_NETWORK);
 	__asm 
 	{
-      mov eax, value; // value копируем в EAX
-      and eax, 1; // берем первый бит из EAX
-      mov value, eax; // результат возвращаем в value
+      mov eax, value
+      and eax, 1
+      mov value, eax
 	}
 
-   // Возвращение результата
 	if (value == 1) val[0] = 1;
 	else  val[0] = 0;
 }
 
+
 // If Hyper Threading is supported then return 1 in second position, else return 0
 extern "C" __declspec(dllexport) void check_availability_HyperThreading(TCHAR* val) 
 {
-   int value;
+   int hyperThreads = 0;
+   unsigned int sup_value, sup_value_2, sup_value_3;
+   char vendor[12];
+   int sup_value_4;
+  
+
+   //Если выполнить cpuid с EAX=0, то:
+   //EAX - максимальное значение, с которым можно выполнять cpuid
+   //EBX:EDX:ECX - строка - 12-байтный идентификатор производителя
+
+
+   // Узнаем производителя процессора
    __asm
    {
+      mov eax, 0
+      cpuid
+      MOV sup_value_4, ECX
+      MOV sup_value_2, EDX
+      mov sup_value, ebx
       mov eax, 1
       cpuid
-      shr eax, 28
-      and edx, 1
-      mov value, edx
+      shr edx, 28
+      mov sup_value_3, edx;
    }
-   
-   // Возвращение результата
-   if (value == 1) val[1] = 1;
+   ((unsigned*)vendor)[0] = sup_value;
+   ((unsigned*)vendor)[1] = sup_value_2;
+   ((unsigned*)vendor)[2] = sup_value_4;
+
+
+   std::string cpuVendor = std::string(vendor, 12);
+   if (cpuVendor == "GenuineIntel" && (sup_value_3 & 1)) // Сравнение с индентификатором EBX (Intel)
+   {
+      __asm
+      {
+         mov eax, 1
+         cpuid
+         mov sup_value, ebx; // >> 16 & 0x0FF Отвечает за лог ядра
+         mov sup_value_2, eax; // >> 26 & 0x3f + 1 Отвечает за ядра
+
+      }
+      if (((sup_value >> 16) & 0x0FF) / (((sup_value_2 >> 26) & 0x3f) + 1))
+         hyperThreads = 1; // здесь уже тру
+   }
+
+   if (hyperThreads == 1) val[1] = 1;
    else val[1] = 0;
 }
--- Управление ресурсами/РГЗ/Main_folder/RGZ_9.cpp
+++ Управление ресурсами/РГЗ/Main_folder/RGZ_9.cpp
@@ -1,29 +1,30 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <windows.h>
+#include <iostream>
 #include <stdio.h>
 #include <string.h>
 
 TCHAR info[256];
 
-/* 
+/*
 If the process completed successfully return 1
 If the process could not load the library return -1
 If the process could not load the function from library return (Network connection) -2
 If the process could not load the function from library return (htt) -3
 */
-DWORD WINAPI get_info(void*) 
-{ 
+DWORD WINAPI get_info(void*)
+{
    typedef int(*ImportFunction)(TCHAR*);
    ImportFunction check_network, check_availability_HyperThreading;
    HINSTANCE h_Lib;
-   
+
    TCHAR info_[2];
 
    if ((h_Lib = LoadLibrary(TEXT("Dll.dll"))) == NULL) // Загружаем библиотеку
       return -1;
-    
+
    // Загружаем функцию из библиотеки, и в случае успеха используем её
-   if (!(check_network = (ImportFunction)GetProcAddress(h_Lib, "check_network"))) 
+   if (!(check_network = (ImportFunction)GetProcAddress(h_Lib, "check_network")))
       return -2;
    else check_network(info_);
 
@@ -45,39 +46,12 @@ LRESULT CALLBACK WindowFunc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
    HDC hDC;
    char c = ' ';
 
-
    switch (msg)
    {
    case WM_CREATE:
       char text_[255];
       char text_sup[30];
       char text_sup_2[30];
-      
-      //////////////////////////////////////////////////// Проверка Hyper-Threading
-   
-      char info_proc[250];
-
-      __asm
-      {
-         mov eax, 0
-         cpuid
-         mov info_proc, ebx;
-      }
-
-      //Если выполнить cpuid с EAX=0, то:
-      //EAX - максимальное значение, с которым можно выполнять cpuid
-      //EBX:EDX:ECX - строка - 12-байтный идентификатор производителя
-
-
-
-
-
-
-
-
-
-
-      ////////////////////////////////////////////////////
 
       HANDLE hThread;
       DWORD IDThread;
@@ -87,13 +61,13 @@ LRESULT CALLBACK WindowFunc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
       WaitForSingleObject(hThread, INFINITE);
       GetExitCodeThread(hThread, &iRetVal); // Получаем код ошибки в iRetVal
       CloseHandle(hThread);
-      
+
       if (iRetVal != 1) // В случае если выход из процесса вернул код ошибки, обрабатываем ошибки
       {
          switch (iRetVal)
          {
          case -1: // Ошибка загрузки библиотеки
-            MessageBox(0, L"Failed to load library!\0", 
+            MessageBox(0, L"Failed to load library!\0",
                L"Error!", MB_ICONERROR);
             break;
          case -2: // Ошибка загрузки функций из библиотеки
@@ -113,7 +87,7 @@ LRESULT CALLBACK WindowFunc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
             strcpy(text_sup, "exist");
          else
             strcpy(text_sup, "absent");
-         
+
          if (info[1] == 1)
             strcpy(text_sup_2, "Yes");
          else
@@ -121,7 +95,7 @@ LRESULT CALLBACK WindowFunc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 
          sprintf(text_, "Network: %s. Support Hyper-Threading: %s.", text_sup, text_sup_2);
       }
-      
+
       c = text_[0];
       for (int i = 1; c != '\0'; i++)
       {
@@ -153,17 +127,17 @@ int WINAPI WinMain(HINSTANCE hThisInst, HINSTANCE, LPSTR, int nCmdShow)
    wcl.lpszClassName = L"My_Class";
    wcl.lpfnWndProc = WindowFunc;
    wcl.hIcon = LoadIcon(NULL, IDI_INFORMATION);
-   wcl.hCursor = LoadCursor(NULL, IDC_ARROW); 
+   wcl.hCursor = LoadCursor(NULL, IDC_ARROW);
    wcl.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
    wcl.style = CS_HREDRAW | CS_VREDRAW;
 
    RegisterClass(&wcl);
-   hWnd = CreateWindow(L"My_Class", 
+   hWnd = CreateWindow(L"My_Class",
       L"Resource management in computing systems",
       WS_OVERLAPPEDWINDOW | WS_CLIPCHILDREN |
       WS_CLIPSIBLINGS, 125, 60, 600, 120,
       HWND_DESKTOP, NULL, hThisInst, NULL);
- 
+
    ShowWindow(hWnd, nCmdShow);
    UpdateWindow(hWnd);
 
